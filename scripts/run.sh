5a9e8c699c012208c8341b90f0a88cb6f800f72b
AnzelaMachackova <angmachy@gmail.com>
2024-05-27 08:49:47 +0200

    saving app output to the json file & debugging

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,11 +11,7 @@ pip install -r requirements.txt
 # run the main application with the GitHub repos passed to the script
 python src/main.py "$@"
 
-echo "Starting Flask app..."
-python src/app.py
+# stoping environment
+deactivate
 
-echo "Press Ctrl+C to stop app and deactivate environment."
-trap 'echo "Stopping Flask server..."; kill $!; echo "Deactivating virtual environment..."; deactivate; echo "Environment closed.";' SIGINT
-
-wait
 echo "The end."
